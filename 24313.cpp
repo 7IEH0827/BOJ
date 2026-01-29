33c53a89e0215303fdae611630dae12e6e1cf513
7IEH0827 <ys740128@naver.com>
2026-01-29 14:20:15 +0900

    feat: 코드 추가 26/1/29

---
+++ 24313.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a1, a0, c, n;
+
+	cin >> a1 >> a0 >> c >> n;
+
+	if (c < a1) cout << 0;
+
+	else {
+		if (a1 * n + a0 > c * n) cout << 0;
+
+		else cout << 1;
+	}
+}
