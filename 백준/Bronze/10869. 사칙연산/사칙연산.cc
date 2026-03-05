d0e414c20c41ee6e5d6d083b5fcaa76579a2f2ec
7IEH <89971268+7IEH0827@users.noreply.github.com>
2026-03-05 21:26:15 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+
+	cin >> A;
+	cin >> B;
+
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B;
+
+	return 0;
+}
