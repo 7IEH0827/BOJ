62c7f8a13bce0ab279f34ff8012dd2c33bc6ecff
7IEH <89971268+7IEH0827@users.noreply.github.com>
2026-03-05 21:33:17 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string S;
+
+	cin >> S;
+
+	for (char i = 'a'; i <= 'z'; i++)
+		cout << (int)S.find(i) << " ";
+
+	return 0;
+}
