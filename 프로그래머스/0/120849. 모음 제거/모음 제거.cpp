d493ffe48a6fafea752fcfcb1591d98b94b39cd5
7IEH <89971268+7IEH0827@users.noreply.github.com>
2026-05-08 13:10:00 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for (int i = 0; i < my_string.size(); ++i) {
+        if (my_string[i] == 97 || my_string[i] == 101 || my_string[i] == 105 || my_string[i] == 111 || my_string[i] == 117)
+            continue;
+        answer += my_string[i];
+    }
+    
+    return answer;
+}
